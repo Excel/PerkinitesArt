9f5a38d35e7c2227301607ad22f18b7f54011e09
John Tran <oblivion.exe@gmail.com>
2012-08-25 04:54:18 -0400

    The data.

---
+++ charEditor/dbData/EnemyData.java
@@ -0,0 +1,12 @@
+   package dbData;
+
+   import java.util.ArrayList;
+
+   public class EnemyData extends UnitData{
+   	
+      public String ai;
+      
+      public EnemyData() {
+      
+      }
+   }
---
+++ charEditor/dbData/UnitData.java
@@ -0,0 +1,20 @@
+   package dbData;
+
+   import java.util.ArrayList;
+
+   public class UnitData {
+      public String name;
+      public String id;
+      public String sprite;
+   
+      public int health;
+      public int defense;
+      public int speed;
+   
+   
+      public ArrayList<AbilityData> abilities;
+      
+      public UnitData() {
+      
+      }
+   }
